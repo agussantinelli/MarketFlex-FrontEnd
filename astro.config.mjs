852bf718979b3dabd02799f08e57cadc03bab44e
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-16 23:50:35 -0300

    Armo Configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import react from '@astrojs/react';
 
 export default defineConfig({
-    integrations: [icon()],
+    integrations: [icon(), react()],
     server: {
         port: 2611,
     },
