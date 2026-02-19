c197e0c052906843cd77fc98ed2e140d81dfc717
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-19 15:15:44 -0300

    NUEVO: Product Service

---
+++ src/services/product.service.ts
@@ -0,0 +1,12 @@
+import { api } from './api';
+import type { Product } from '../types/product.types';
+
+export const getProducts = async (): Promise<Product[]> => {
+    try {
+        const response: { data: Product[] } = await api.get('products').json();
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        return [];
+    }
+};
