5d7d725a74dccd26b8a4f055b63bd216dd2d2616
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-20 20:20:11 -0300

    Incorporo Plugin Basic SSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     integrations: [icon(), react()],
     output: 'server',
     server: {
         port: 2611,
+    },
+    vite: {
+        plugins: [basicSsl()],
     },
 });
