5059615f68687162b46105cb8b828711ee7e1464
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-25 00:32:53 -0300

    Actualizo Script

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,8 +44,9 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npm run dev',
+        command: 'pnpm dev',
         url: 'http://localhost:4321',
         reuseExistingServer: !process.env.CI,
+        timeout: 120000,
     },
 });
