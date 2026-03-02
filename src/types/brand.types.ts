6a40a20490b588630a08cdd5487e5fd96fbc3ab6
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-01 22:26:47 -0300

    Actualizo Brand Types

--- src/types/brand.types.ts
+++ src/types/brand.types.ts
@@ -1,9 +1,25 @@
-export type Brand = {
+export interface Brand {
     id: string;
     nombre: string;
-};
+}
 
-export type BrandsResponse = {
+export interface BrandWithCount extends Brand {
+    productCount: number;
+}
+
+export interface BrandProductSummary {
+    id: string;
+    nombre: string;
+    foto: string | null;
+    valor: string | null;
+}
+
+export interface BrandsResponse {
     status: string;
-    data: Brand[];
-};
+    data: BrandWithCount[];
+}
+
+export interface BrandProductsResponse {
+    status: string;
+    data: BrandProductSummary[];
+}
