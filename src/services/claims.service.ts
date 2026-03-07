969bf07358c9b0e285c82c8b8d034d93bc55cde0
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-07 16:15:02 -0300

    Agrego Endpoint Crear Reclamo

--- src/services/claims.service.ts
+++ src/services/claims.service.ts
@@ -5,5 +5,9 @@ export const claimsService = {
     getAll: async (): Promise<Claim[]> => {
         const response = await api.get('claims').json<{ status: string; data: Claim[] }>();
         return response.data;
+    },
+    create: async (data: { compraId: string; motivo: string; descripcion: string }): Promise<Claim> => {
+        const response = await api.post('claims', { json: data }).json<{ status: string; data: Claim }>();
+        return response.data;
     }
 };
